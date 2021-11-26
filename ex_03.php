a42096092f12efbb267e7c15bcd3c8d5b071eb57
hugo <hugo.boyer@epitech.eu>
2021-11-26 12:51:49 +0100

    test a

--- ex_03.php
+++ ex_03.php
@@ -1,12 +1,15 @@
 <?php
-function resum_join_str($sta = FALSE, $stb = FALSE)
+function resum_join_str($sta = NULL, $stb = NULL)
 {
-        if (strlen($sta) < 14 || strlen($stb) < 3) {
-              str_pad($sta, 15, '.');
-              str_pad($sta, 15, '.');
+        if (strlen($sta) < 14 || strlen($stb) < 3 && $sta !== FALSE || $stb !== FALSE) {
+          return    str_pad($sta, 14,'.');
+          return    str_pad($stb,3, '.',STR_PAD_LEFT);
 
-            substr($sta, 14);
+             substr($sta, 14);
               substr($stb, -3);
-        } 
+        } else {
+           return FALSE;
+        }
+       
 }
-var_dump(resum_join_str("gjeijg", 'a'));
+
